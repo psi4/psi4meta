39b2f5c8feadca251b2d6dea5d3bf843e7bd9447
Lori A. Burns <lori.burns@gmail.com>
2016-01-15 03:06:28 -0500

    pcmsolver apparently working. old csx4psi plugin checked in for historical record.

--- conda-recipes/pcmsolver/build.sh
+++ conda-recipes/pcmsolver/build.sh
@@ -1,22 +1,22 @@
 # load Intel compilers and mkl
 source /theoryfs2/common/software/intel2015/bin/compilervars.sh intel64
 # force static link to Intel mkl, except for openmp
-MKLROOT=/theoryfs2/common/software/intel2015/composer_xe_2015.0.090/mkl/lib/intel64
-LAPACK_INTERJECT="${MKLROOT}/libmkl_intel_lp64.a ${MKLROOT}/libmkl_intel_thread.a ${MKLROOT}/libmkl_core.a -liomp5 -lm"
+MKLROOT=/theoryfs2/common/software/intel2015/composer_xe_2015.3.187/mkl/lib/intel64
+COMPROOT=${MKLROOT}/../../../compiler/lib/intel64
+LAPACK_INTERJECT="${COMPROOT}/libifport.a ${COMPROOT}/libifcore_pic.a"
+# -DLIBC_INTERJECT="${LIBC_INTERJECT}" \  # earlier pcmsolver
+#    -DLAPACK_LIBRARIES="${LIBC_INTERJECT} ${LAPACK_INTERJECT}" \  # chemps2
+#LAPACK_INTERJECT="${MKLROOT}/libmkl_intel_lp64.a ${MKLROOT}/libmkl_intel_thread.a ${MKLROOT}/libmkl_core.a -liomp5 -lm"  # chemps2
 # link against older libc for generic linux
 TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/psi4cmake/psi4/glibc2.12rpm
 LIBC_INTERJECT="-L${TLIBC}/usr/lib64 ${TLIBC}/lib64/libpthread.so.0 ${TLIBC}/lib64/libc.so.6"
 # round off with pre-detected dependencies
-#MCONDA=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/gslenv/lib
+MCONDA=${PREFIX}/lib
 #GSL_INTERJECT="-L${MCONDA};-lhdf5;-lhdf5_hl;-lhdf5;-lpthread;-lz;-lrt;-ldl;-lm"
 #HDF5_INTERJECT="-L${MCONDA};-lgsl;-lgslcblas;-lm"
-# redistribute the Intel openmp
-REDIST=${MKLROOT}/../../../compiler/lib/intel64/libiomp5.so
 
 mkdir build
 cd build
-mkdir lib
-cp ${REDIST} ${PREFIX}/lib
 cmake \
  -DCMAKE_CXX_COMPILER=icpc \
  -DCMAKE_C_COMPILER=icc \
@@ -41,10 +41,12 @@ cmake \
  -DENABLE_GENERIC=ON \
  -DENABLE_TIMER=OFF \
  -DBUILD_STANDALONE=OFF \
+ -DLIBC_INTERJECT="${LIBC_INTERJECT} ${LAPACK_INTERJECT}" \
+ -DZLIB_LIBRARIES= ${PREFIX} \
  -DENABLE_DOCS=OFF \
  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
  -DCMAKE_INSTALL_LIBDIR=lib \
  ${SRC_DIR}
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} VERBOSE=1
 make install
 
