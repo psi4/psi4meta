b5b31f30c03ac275e1aa6ef3f69ad74f49e0c6a8
Lori A. Burns <lori.burns@gmail.com>
2016-01-19 06:39:17 -0500

    working psi4 recipe with pcmsolver and chemps2

--- conda-recipes/psi4/build.sh
+++ conda-recipes/psi4/build.sh
@@ -1,10 +1,6 @@
 source /theoryfs2/common/software/intel2015/bin/compilervars.sh intel64
 export TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/nightly/glibc2.12
 
-    #-DBOOST_INCLUDEDIR=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/boostenv/include \
-    #-DBOOST_LIBRARYDIR=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/boostenv/lib \
-    #-DENABLE_PCMSOLVER=ON \ works but not static enough for conda
-
 mkdir build
 cd build
 cmake \
@@ -15,8 +11,16 @@ cmake \
     -DLIBINT_OPT_AM=6 \
     -DENABLE_STATIC_LINKING=ON \
     -DENABLE_XHOST=OFF \
+    -DDETECT_EXTERNAL_STATIC=ON \
+    -DENABLE_CONDA_DEST=ON \
     -DENABLE_PLUGINS=ON \
-    -DENABLE_CHEMPS2=OFF \
+    -DENABLE_CHEMPS2=ON \
+    -DCHEMPS2_ROOT=${PREFIX} \
+    -DZLIB_ROOT=${PREFIX} \
+    -DGSL_ROOT_DIR=${PREFIX} \
+    -DHDF5_ROOT=${PREFIX} \
+    -DENABLE_PCMSOLVER=ON \
+    -DPCMSOLVER_ROOT=${PREFIX} \
     -DSPHINX_ROOT=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/boostenv \
     -DCMAKE_BUILD_TYPE=release \
     -DLIBC_INTERJECT="-L${TLIBC}/usr/lib64 ${TLIBC}/lib64/libpthread.so.0 ${TLIBC}/lib64/libc.so.6" \
@@ -28,4 +32,8 @@ make -j3  #-j${CPU_COUNT}  # get incomplete build at full throttle
 make sphinxman
 make ghfeed
 make install
+
+# -DBOOST_INCLUDEDIR=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/boostenv/include
+# -DBOOST_LIBRARYDIR=/theoryfs2/ds/cdsgroup/psi4-install/miniconda/envs/boostenv/lib
+# -DCMAKE_BUILD_TYPE=release
 
