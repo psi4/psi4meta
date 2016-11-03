6399651f3ca291a917dd5d7ec89d1e3d2add282a
Psinet for mac <mac.psinet@gmail.com>
2016-11-03 03:46:30 -0400

    mac psi4 working with clang

--- conda-recipes/psi4/build.sh
+++ conda-recipes/psi4/build.sh
@@ -1,58 +1,84 @@
 
 if [ "$(uname)" == "Darwin" ]; then
 
-    export DYLD_LIBRARY_PATH=${PREFIX}/lib:${DYLD_LIBRARY_PATH}
-    rm ${PREFIX}/lib/libsqlite3*
+    #export DYLD_LIBRARY_PATH=${PREFIX}/lib:${DYLD_LIBRARY_PATH}
+    #rm ${PREFIX}/lib/libsqlite3*
 
     # configure
     ${PREFIX}/bin/cmake \
-        -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-        -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
-        -DCMAKE_Fortran_COMPILER=${PREFIX}/bin/gfortran \
-        -DCMAKE_OSX_DEPLOYMENT_TARGET='' \
-        -DPYTHON_INTERPRETER=${PYTHON} \
-        -DLIBINT_OPT_AM=6 \
-        -DENABLE_STATIC_LINKING=OFF \
-        -DENABLE_CONDA_DEST=ON \
-        -DENABLE_PLUGINS=OFF \
-        -DBUILD_CUSTOM_BOOST=ON \
-        -DENABLE_CHEMPS2=ON \
-        -DENABLE_PCMSOLVER=ON \
-        -DENABLE_AMBIT=ON \
-        -DZLIB_ROOT=${PREFIX} \
-        -DHDF5_ROOT=${PREFIX} \
-        -DCHEMPS2_ROOT=${PREFIX} \
-        -DPCMSOLVER_ROOT=${PREFIX} \
-        -DAMBIT_ROOT=${PREFIX} \
-        -DCMAKE_BUILD_TYPE=debug \
+        -H${SRC_DIR} \
+        -Bbuild \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DEXECUTABLE_SUFFIX= \
-        -DENABLE_MPI=OFF \
-        -DENABLE_SGI_MPT=OFF \
-        -DENABLE_OMP=ON \
-        -DENABLE_VECTORIZATION=OFF \
-        -DENABLE_CSR=OFF \
-        -DENABLE_SCALAPACK=OFF \
-        -DENABLE_SCALASCA=OFF \
-        -DENABLE_UNIT_TESTS=OFF \
-        -DENABLE_CXX11_SUPPORT=ON \
-        ${SRC_DIR}
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=clang \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+        -DCMAKE_Fortran_COMPILER=${PREFIX}/bin/gfortran \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DPYMOD_INSTALL_LIBDIR="/python2.7/site-packages" \
+        -DBUILD_SHARED_LIBS=ON \
+        -DENABLE_gdma=ON \
+        -DMAX_AM_ERI=6 \
+        -DPYTHON_EXECUTABLE=${PYTHON} \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DBUILDNAME=LAB-OSX-clang7.3.0-accelerate-release-conda \
+        -DSITE=gatech-mac-conda \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=''
+
+#    @rpath/core.so (compatibility version 0.0.0, current version 0.0.0)
+#    @rpath/libpython2.7.dylib (compatibility version 2.7.0, current version 2.7.0)
+#    /usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1226.10.1)
+#    @rpath/libgdma.dylib (compatibility version 0.0.0, current version 0.0.0)
+#    @rpath/libderiv.dylib (compatibility version 0.0.0, current version 0.0.0)
+#    @rpath/libint.dylib (compatibility version 0.0.0, current version 0.0.0)
+#    @rpath/libefp.dylib (compatibility version 0.0.0, current version 0.0.0)
+#    /System/Library/Frameworks/Accelerate.framework/Versions/A/Accelerate (compatibility version 1.0.0, current version 4.0.0)
+#    /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libLAPACK.dylib (compatibility version 1.0.0, current version 1.0.0)
+#    /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib (compatibility version 1.0.0, current version 1.0.0)
+#    /usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 120.1.0)
+
+        #-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        #-DCMAKE_BUILD_TYPE=Release \
+        #-DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+        #-DCMAKE_INSTALL_LIBDIR=lib \
+        #-DPYMOD_INSTALL_LIBDIR="/python2.7/site-packages" \
+        #-DMAX_AM_ERI=4 \
+        #-DPYTHON_EXECUTABLE=${PYTHON} \
+        #-DCMAKE_PREFIX_PATH="${PREFIX}" \
+        #-DBUILDNAME=LAB-OSX-clang7.3.0-accelerate-release-conda \
+        #-DSITE=gatech-mac-conda \
+        #-DCMAKE_OSX_DEPLOYMENT_TARGET='' \
+        #-DLAPACK_LIBRARIES="/System/Library/Frameworks/Accelerate.framework/Accelerate" \
+        #-DBLAS_LIBRARIES="/System/Library/Frameworks/Accelerate.framework/Accelerate"
+
+
+        #-DENABLE_OPENMP=ON \
+        #-DCMAKE_C_COMPILER=cc \
+        #-DCMAKE_CXX_COMPILER=c++ \
+#        -DBUILD_SHARED_LIBS=ON \
+
+#        -DCMAKE_Fortran_COMPILER=${PREFIX}/bin/gfortran \
+#        -DMAX_AM_ERI=6 \
+#        -DPYTHON_EXECUTABLE=${PYTHON} \
+#        -DENABLE_gdma=ON \
 
     # build
-    make
-    make psi4so
+    cd build
+    make -j${CPU_COUNT}
 
     # install
     make install
 
     # test
-    install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib ${PREFIX}/bin/psi4
-    install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib ${PREFIX}/lib/python2.7/site-packages/psi4.so
-    sed -i.bak "s|/opt/anaconda1anaconda2anaconda3|${PREFIX}|g" ${PREFIX}/share/psi4/python/pcm_placeholder.py
-    DYLD_LIBRARY_PATH=${PREFIX}/lib:$DYLD_LIBRARY_PATH \
-           PYTHONPATH=${PREFIX}/bin:${PREFIX}/lib/python2.7/site-packages:$PYTHONPATH \
-                 PATH=${PREFIX}/bin:$PATH \
-        ctest -L quick
+    #install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib ${PREFIX}/bin/psi4
+    #install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib ${PREFIX}/lib/python2.7/site-packages/psi4.so
+    #sed -i.bak "s|/opt/anaconda1anaconda2anaconda3|${PREFIX}|g" ${PREFIX}/share/psi4/python/pcm_placeholder.py
+    #DYLD_LIBRARY_PATH=${PREFIX}/lib:$DYLD_LIBRARY_PATH \
+    #       PYTHONPATH=${PREFIX}/bin:${PREFIX}/lib/python2.7/site-packages:$PYTHONPATH \
+    #             PATH=${PREFIX}/bin:$PATH \
+    ctest -M Nightly -T Test -T Submit -j${CPU_COUNT} -L quick
+      # TODO drop quick when all passing again
+        #ctest -L quick
 
     # test debug line
     #DYLD_LIBRARY_PATH=/Users/loriab/linux/psi4-build/minicondadrive/envs/_build_placehold_placehold_pl/lib PATH=/Users/loriab/linux/psi4-build/minicondadrive/envs/_build_placehold_placehold_pl/bin:$PATH PYTHONPATH=/Users/loriab/linux/psi4-build/minicondadrive/envs/_build_placehold_placehold_pl/lib/python2.7/site-packages/:$PYTHONPATH
