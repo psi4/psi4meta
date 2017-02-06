0e05418e99b8ea37a53855e7469c50e989fe4585
Lori A. Burns <lori.burns@gmail.com>
2017-02-05 19:57:28 -0500

    v2rdm_casscf recipe for Linux, now with CMake, not configure

---
+++ conda-recipes/v2rdm/build.sh
@@ -0,0 +1,38 @@
+if [ "$(uname)" == "Linux" ]; then
+
+    # load Intel compilers and mkl
+    set +x
+    source /theoryfs2/common/software/intel2016/bin/compilervars.sh intel64
+    set -x
+
+    # link against older libc for generic linux
+    TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/nightly/glibc2.12
+    LIBC_INTERJECT="-liomp5;${TLIBC}/lib64/libc.so.6;-Wl,-Bstatic;-Wl,--whole-archive;-lifport;-lifcoremt_pic;-Wl,--no-whole-archive;-Wl,-Bdynamic"
+
+    ${PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -B. \
+        -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=icc \
+        -DCMAKE_CXX_COMPILER=icpc \
+        -DCMAKE_Fortran_COMPILER=ifort \
+        -DBUILD_SHARED_LIBS=ON \
+        -DENABLE_OPENMP=ON \
+        -DENABLE_XHOST=OFF \
+        -DENABLE_GENERIC=ON \
+        -DLIBC_INTERJECT=${LIBC_INTERJECT} \
+        -DCMAKE_C_FLAGS=" -qopenmp -static-libgcc -static-intel -wd10237" \
+        -DCMAKE_CXX_FLAGS=" -qopenmp -Wl,--as-needed -static-libstdc++ -static-libgcc -static-intel -wd10237" \
+        -DCMAKE_Fortran_FLAGS=" -qopenmp -static-libgcc -static-intel -wd10006"
+fi
+
+# build
+make -j${CPU_COUNT}
+#make VERBOSE=1
+
+# install
+make install
+
+# Note: to build from psi4 package cache
+#        -C ${PREFIX}/share/cmake/psi4/psi4PluginCache.cmake \
---
+++ conda-recipes/v2rdm/post-link.sh
@@ -0,0 +1,14 @@
+set +x
+
+echo "" >> .messages.txt
+echo "  Thank you for installing the v2rdm_casscf plugin to Psi4." >> .messages.txt
+echo "    GitHub:  github.com/edeprince3/v2rdm_casscf" >> .messages.txt
+echo "    Binary:  anaconda.org/psi4/v2rdm_casscf" >> .messages.txt
+echo "    Inputs:  ${SP_DIR}/v2rdm_casscf/tests" >> .messages.txt
+echo "    Test (after first activating conda installation or environment):" >> .messages.txt
+if [ "${CONDA_PY}" == "27" ]; then
+    echo "      psi4 \"\$(dirname \$(which psi4))\"/../lib/python2.7/site-packages/v2rdm_casscf/tests/v2rdm1/input.dat -o output.dat" >> .messages.txt
+elif [ "${CONDA_PY}" == "35" ]; then
+    echo "      psi4 \"\$(dirname \$(which psi4))\"/../lib/python3.5/site-packages/v2rdm_casscf/tests/v2rdm1/input.dat -o output.dat" >> .messages.txt
+fi
+echo "" >> .messages.txt
