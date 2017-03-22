3a1d13b464bedb47d0cec9636f1a12c836d8502a
Lori A. Burns <lori.burns@gmail.com>
2017-03-22 00:19:27 -0400

    update docs shuffling machinery

--- psicode-feedsfrom-psinet/handle_sphinxman.sh
+++ psicode-feedsfrom-psinet/handle_sphinxman.sh
@@ -1,6 +1,7 @@
 #!/bin/sh  
 DOCSTAR=~/machinations/cb-sphinxman.tar.gz
 FEEDSTAR=~/machinations/cb-feed.tar.gz
+DOXYSTAR=~/machinations/cb-doxyman.tar.gz
 # */15 * * * * bash ~/machinations/handle_sphinxman.sh >>~/machinations/ct.log 2>&1
 
 echo ""
@@ -33,6 +34,20 @@ if [ -f "$FEEDSTAR" ]; then
     echo "handle_sphinxman.sh: removing tarball"
     rm $FEEDSTAR
 fi
-echo "handle_sphinxman.sh: completing"
 
+echo "handle_sphinxman.sh: starting on $DOXYSTAR"
+if [ -f "$DOXYSTAR" ]; then
+
+    echo "handle_sphinxman.sh: shifting aside existing doxy"
+    rm -rf ~/html/psi4manual/doxymaster-but-one/
+    mv ~/html/psi4manual/doxymaster/ ~/html/psi4manual/doxymaster-but-one/
+
+    echo "handle_sphinxman.sh: unpacking new doxy"
+    tar -zxf $DOXYSTAR -C ~/html/psi4manual/
+
+    echo "handle_sphinxman.sh: removing tarball"
+    rm $DOXYSTAR
+fi
+
+echo "handle_sphinxman.sh: completing"
 
