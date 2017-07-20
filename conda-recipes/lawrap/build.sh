5ffdca04a869d8b939f77b5849d9af9266d08d58
Psinet for mac <mac.psinet@gmail.com>
2017-07-20 09:44:20 -0400

    mac lawrap

---
+++ conda-recipes/lawrap/build.sh
@@ -0,0 +1,21 @@
+# configure
+${PREFIX}/bin/cmake \
+    -H${SRC_DIR} \
+    -Bbuild \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
+    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_INSTALL_LIBDIR=lib
+
+# build
+cd build
+make -j${CPU_COUNT}
+#make VERBOSE=1
+
+# install
+make install
+
+# test
+# no independent tests
+
