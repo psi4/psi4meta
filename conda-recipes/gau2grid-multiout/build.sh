1833a11d0d059cbfe316082b34f618c4ae261907
Psinet for mac <mac.psinet@gmail.com>
2018-06-08 17:34:15 -0400

    M: intel g2g

--- conda-recipes/gau2grid-multiout/build.sh
+++ conda-recipes/gau2grid-multiout/build.sh
@@ -1,4 +1,28 @@
 
+if [ "$(uname)" == "Darwin" ]; then
+
+    ALLOPTS="${OPTS}"
+
+    # configure
+    ${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=icc \
+        -DCMAKE_C_FLAGS="${ALLOPTS}" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DPYMOD_INSTALL_LIBDIR="${PYMOD_INSTALL_LIBDIR}" \
+        -DINSTALL_PYMOD=ON \
+        -DBUILD_SHARED_LIBS=ON \
+        -DENABLE_XHOST=OFF \
+        -DENABLE_GENERIC=OFF \
+        -DPYTHON_EXECUTABLE=${PYTHON} \
+        -DMAX_AM=8
+
+fi
+
+
 if [ "$(uname)" == "Linux" ]; then
 
     # load Intel compilers
