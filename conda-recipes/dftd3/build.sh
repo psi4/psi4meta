ca9fa63813cff5caffb23707fbb3b30727cdb55d
Lori A. Burns <lori.burns@gmail.com>
2019-02-01 16:41:08 -0500

    L: dftd3 3.2.1, rebuild libxc w/new c-b

--- conda-recipes/dftd3/build.sh
+++ conda-recipes/dftd3/build.sh
@@ -1,8 +1,26 @@
 
 if [ "$(uname)" == "Darwin" ]; then
 
-    make FC='${GFORTRAN}' LINKER='${GFORTRAN}'
+    # for FortranCInterface
+    CMAKE_Fortran_FLAGS="${FFLAGS} -L${CONDA_BUILD_SYSROOT}/usr/lib/system/ ${OPTS}"
+
+    # configure
+    ${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_C_COMPILER=${CLANG} \
+        -DCMAKE_Fortran_COMPILER=${GFORTRAN} \
+        -DCMAKE_C_FLAGS="${CFLAGS} ${OPTS}" \
+        -DCMAKE_Fortran_FLAGS="${CMAKE_Fortran_FLAGS}" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DBUILD_SHARED_LIBS=ON \
+        -DENABLE_OPENMP=ON \
+        -DOpenMP_C_FLAG="-fopenmp=libiomp5" \
+        -DENABLE_XHOST=OFF
 fi
+
 
 if [ "$(uname)" == "Linux" ]; then
 
@@ -11,10 +29,26 @@ if [ "$(uname)" == "Linux" ]; then
     source /theoryfs2/common/software/intel2018/bin/compilervars.sh intel64
     set -x
 
-    make FC='ifort' LINKER='ifort -static'
+    # link against conda GCC
+    ALLOPTS="-gnu-prefix=${HOST}- ${OPTS}"
+
+    # configure
+    ${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_Fortran_COMPILER=ifort \
+        -DCMAKE_Fortran_FLAGS="${ALLOPTS} -static" \
+        -DENABLE_XHOST=OFF
 fi
 
-# pseudo "make install"
-mkdir -p ${PREFIX}/bin
-cp dftd3 ${PREFIX}/bin
+# build
+cd build
+make -j${CPU_COUNT}
 
+# install
+make install
+
+# test
+# no independent tests
