c6e395a45074ab443d4fdfb04272af49005d16e6
Psinet for mac <mac.psinet@gmail.com>
2019-04-12 02:33:51 -0400

    M: rebuild v2rdm after parent_symm. add mp2d recipe

--- conda-recipes/mp2d/build.sh
+++ conda-recipes/mp2d/build.sh
@@ -1,8 +1,17 @@
 
-#if [ "$(uname)" == "Darwin" ]; then
-#
-#    make FC='${GFORTRAN}' LINKER='${GFORTRAN}'
-#fi
+if [ "$(uname)" == "Darwin" ]; then
+
+    # configure
+    ${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_COMPILER=${CLANGXX} \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
+        -DENABLE_XHOST=OFF
+
+fi
 
 if [ "$(uname)" == "Linux" ]; then
 
@@ -22,6 +31,7 @@ if [ "$(uname)" == "Linux" ]; then
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_COMPILER=icpc \
         -DCMAKE_CXX_FLAGS="${ALLOPTS} -static"
+#needed?        -DENABLE_XHOST=OFF
 fi
 
 # build
