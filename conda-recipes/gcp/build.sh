9e01ac09fad031e0cd6c6915935c1d7d3a4d82a5
Lori A. Burns <lori.burns@gmail.com>
2020-03-14 20:47:51 -0400

    L: switch gcp to cmake

--- conda-recipes/gcp/build.sh
+++ conda-recipes/gcp/build.sh
@@ -2,6 +2,10 @@
 if [ "$(uname)" == "Darwin" ]; then
 
     make FC='${GFORTRAN}' LINKER='${GFORTRAN}'
+
+    # pseudo "make install"
+    mkdir -p ${PREFIX}/bin
+    cp gcp ${PREFIX}/bin
 fi
 
 if [ "$(uname)" == "Linux" ]; then
@@ -12,11 +16,36 @@ if [ "$(uname)" == "Linux" ]; then
     set -x
 
     make FC='ifort' LINKER='ifort'
-fi
 
-# pseudo "make install"
-mkdir -p ${PREFIX}/bin
-cp gcp ${PREFIX}/bin
+
+    # load Intel compilers
+    set +x
+    source /theoryfs2/common/software/intel2018/bin/compilervars.sh intel64
+    set -x
+
+    # link against conda GCC
+    ALLOPTS="-O3 -gnu-prefix=${HOST}- ${OPTS}"
+
+    # configure
+    ${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_Fortran_COMPILER=ifort \
+        -DCMAKE_Fortran_FLAGS="${ALLOPTS}" \
+        -DENABLE_XHOST=OFF
+
+    # build
+    cd build
+    make -j${CPU_COUNT}
+
+    # install
+    make install
+
+    # test
+    # no independent tests
+fi
 
 # History
 
