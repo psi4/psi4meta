38d1e55ee9c40860cbd53d68e574fd121d8885f7
Psinet for mac <mac.psinet@gmail.com>
2020-10-28 01:13:15 -0400

    M: merge L&M libint2 at _1.tgz

--- conda-recipes/libint2/build.sh
+++ conda-recipes/libint2/build.sh
@@ -5,7 +5,9 @@ if [ "$(uname)" == "Darwin" ]; then
 #    # * -fno-exceptions squashes `___gxx_personality_v0` symbol and thus libc++ dependence
 #    ALLOPTS="-clang-name=${CLANG} ${OPTS} -fno-exceptions"
 #    ALLOPTSCXX="-clang-name=${CLANG} -clangxx-name=${CLANGXX} -stdlib=libc++ -I${PREFIX}/include/c++/v1 ${OPTS} -fno-exceptions -mmacosx-version-min=10.9"
-    ALLOPTS="-clang-name=${CLANG} -clangxx-name=${CLANGXX} -stdlib=libc++ -I${PREFIX}/include/c++/v1 ${OPTS} -mmacosx-version-min=10.9"
+#    ALLOPTS="-clang-name=${CLANG} -clangxx-name=${CLANGXX} -stdlib=libc++ -I${PREFIX}/include/c++/v1 ${OPTS} -mmacosx-version-min=10.9"
+#        -DCMAKE_CXX_COMPILER=icpc \
+#        -DCMAKE_CXX_FLAGS="${ALLOPTS}" \
 
     # configure
     ${BUILD_PREFIX}/bin/cmake \
@@ -27,28 +29,7 @@ if [ "$(uname)" == "Darwin" ]; then
         -DEigen3_ROOT=${PREFIX} \
         -DENABLE_FORTRAN=OFF \
         -DBUILD_TESTING=ON
-
-# _0
-#        -DENABLE_ERI=1 \
-#        -DENABLE_ERI3=1 \
-#        -DENABLE_ERI2=1 \
-#        -DWITH_ERI_MAX_AM:STRING="4;3" \
-#        -DWITH_ERI3_MAX_AM=4 \
-#        -DWITH_ERI2_MAX_AM=4 \
-
-#        -DCMAKE_CXX_COMPILER=icpc \
-#        -DCMAKE_CXX_FLAGS="${ALLOPTS}" \
-#        -DENABLE_ERI=2 \
-#        -DENABLE_ERI3=2 \
-#        -DENABLE_ERI2=2 \
-#        -DWITH_ERI_MAX_AM:STRING="3;3;3" \
-#        -DWITH_ERI3_MAX_AM=4 \
-#        -DWITH_ERI2_MAX_AM=4 \
-
 fi
-        #-DWITH_ERI_MAX_AM:STRING="5;5;5" \
-        #-DWITH_ERI3_MAX_AM=6 \
-        #-DWITH_ERI2_MAX_AM=6 \
 
 if [ "$(uname)" == "Linux" ]; then
 
@@ -56,6 +37,11 @@ if [ "$(uname)" == "Linux" ]; then
     set +x
     source /theoryfs2/common/software/intel2019/bin/compilervars.sh intel64
     set -x
+
+    # allow more arguments to avoid the dreaded `x86_64-conda_cos6-linux-gnu-ld: Argument list too long` upon linking
+    echo `getconf ARG_MAX`
+    ulimit -s 65535
+    echo `getconf ARG_MAX`
 
     # link against conda GCC
     ALLOPTS="-gnu-prefix=${HOST}- ${OPTS}"
@@ -75,12 +61,6 @@ if [ "$(uname)" == "Linux" ]; then
         -DLIBINT2_SHGAUSS_ORDERING=gaussian \
         -DLIBINT2_CARTGAUSS_ORDERING=standard \
         -DLIBINT2_SHELL_SET=standard \
-        -DENABLE_ERI=2 \
-        -DENABLE_ERI3=2 \
-        -DENABLE_ERI2=2 \
-        -DWITH_ERI_MAX_AM:STRING="5;5;5" \
-        -DWITH_ERI3_MAX_AM=6 \
-        -DWITH_ERI2_MAX_AM=6 \
         -DERI3_PURE_SH=OFF \
         -DERI2_PURE_SH=OFF \
         -DMPFR_ROOT=${PREFIX} \
@@ -91,10 +71,23 @@ if [ "$(uname)" == "Linux" ]; then
         -DENABLE_CXX11API=ON \
         -DENABLE_FORTRAN=OFF \
         -DBUILD_TESTING=ON
-
-        #-DEigen3_DIR=${PREFIX}/share/eigen3/cmake/
 fi
 
 # build & install & test
 cd build
 cmake --build . --target install -j${CPU_COUNT}
+
+
+# This works for making a conda package out of a pre-built install
+#
+if [ "$(uname)" == "SpecialLinux" ]; then
+
+PREBUILT=/psi/gits/libint2/install40
+
+cp -pR ${PREBUILT}/* ${PREFIX}
+cp ${PREFIX}/lib/pkgconfig/libint2.pc tmp0
+sed "s|$PREBUILT|/opt/anaconda1anaconda2anaconda3|g" tmp0 > tmp1
+cp tmp1 ${PREFIX}/lib/pkgconfig/libint2.pc
+
+fi
+
